3c25eabd1bf1d5c54ae95eebd01ebfd8370b878d
Brute1996 <97292099+Brute1996@users.noreply.github.com>
2022-12-18 18:51:07 +0000

    Deploying to gh-pages from @ Brute1996/goit-js-hw-10@ace6712f16a4f227437b8546cdc74b71d23b6d03 🚀

--- index.29a833fa.js
+++
@@ -1,2 +0,0 @@
-var t;(t="germany",fetch(`https://restcountries.com/v3.1/name/${t}`).then((t=>t.json))).then((t=>t)).catch((t=>t));
-//# sourceMappingURL=index.29a833fa.js.map
---
+++ index.97e755d8.js
@@ -0,0 +1,2 @@
+!function(){var n;n="https://restcountries.com/v3.1/name/".concat("germany"),fetch(n).then((function(n){return n.json()})).then((function(n){return console.log(n)})).catch((function(n){return n}))}();
+//# sourceMappingURL=index.97e755d8.js.map
