d0d39d498c0719c93c266082a5f226df72b9db14
Sergey <sergey.cherenkov96@gmail.com>
2022-12-18 20:32:02 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './css/styles.css';
+import fetchCountries from './js/fetchCountries'
 
 const DEBOUNCE_DELAY = 300;
+
+
+fetchCountries('germany')
+    .then(country => country)
+    .catch(error => error)
+
+
