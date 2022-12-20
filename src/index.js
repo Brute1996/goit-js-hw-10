f9e4777c6b489f80e089f529a3c68096910bf40a
Serhii <97292099+Brute1996@users.noreply.github.com>
2022-12-21 00:00:17 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
+import debounce from 'lodash.debounce';
 import './css/styles.css';
 import fetchCountries from './js/fetchCountries'
-import { Report } from 'notiflix/build/notiflix-report-aio';
+
 
 const DEBOUNCE_DELAY = 300;
 
 const searchBox = document.querySelector('#search-box')
 
-searchBox.addEventListener('input', (e) => {
-    fetchCountries(e.currentTarget.value)
-})
+searchBox.addEventListener('input', debounce((e) => {
+    fetchCountries(searchBox.value)
+}, DEBOUNCE_DELAY))
 
 
 
